740f7eef6678315b92c4c5e8852199f08f82df53
Vinicius Abreu <vinicius.goncalves@dtidigital.com.br>
2022-08-16 11:30:51 -0300

    Adicionando casos de erro

--- tarefas-controller.js
+++ tarefas-controller.js
@@ -2,15 +2,25 @@ var Tarefas = require("./tarefa-model");
 
 exports.listarTarefas = (req, res) => {
   Tarefas.find({}, function (err, tarefas) {
-    if (err) return next(err);
-    return res.json(tarefas);
+    if (err) {
+      return res
+        .status(500)
+        .send("Erro no servidor, tente novamente mais tarde.");
+    } else if (tarefas.length == 0) {
+      return res.status(204).send("Nenhuma tarefa cadastrada.");
+    } else {
+      return res.json(tarefas);
+    }
   });
 };
 
 exports.buscarTarefa = (req, res) => {
   Tarefas.findById(req.params.id, function (err, tarefa) {
-    if (err) return next(err);
-    return res.json(tarefa);
+    if (err) {
+      res.status(400).send("Identificador não encontrado.");
+    } else {
+      return res.json(tarefa);
+    }
   });
 };
 
@@ -22,16 +32,20 @@ exports.cadastrarTarefa = (req, res) => {
   });
   tarefa.save(function (err) {
     if (err) {
-      return next(err);
+      res.status(400).send("Todos os dados devem ser passados.");
+    } else {
+      res.send("Tarefa cadastrada com sucesso!");
     }
   });
-  res.send("Tarefa cadastrada com sucesso!");
 };
 
 exports.removerTarefa = (req, res) => {
   Tarefas.findByIdAndRemove(req.params.id, function (err, tarefa) {
-    if (err) next(err);
-    res.send("Tarefa removida com sucesso!");
+    if (err) {
+      res.status(400).send("Identificador não encontrado.");
+    } else {
+      res.send("Tarefa removida com sucesso!");
+    }
   });
 };
 
@@ -49,8 +63,11 @@ exports.alterarTarefa = (req, res) => {
       completa: tarefa.completa,
     },
     function (err, docs) {
-      if (err) next(err);
-      res.send("Tarefa atualizada com sucesso!");
+      if (err) {
+        res.status(400).send("Identificador não encontrado.");
+      } else {
+        res.send("Tarefa atualizada com sucesso!");
+      }
     }
   );
 };
